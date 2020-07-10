48d6660dee91af56d6862dca8c585d67046fcac1
kanari3 <kanari333333@gmail.com>
2020-07-10 13:00:45 +0900

    create basic loadmore listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChangeNotifierProvider(
+        create: (context) => LoadMoreModel(),
+        child: LoadMoreScreen(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class LoadMoreModel extends ChangeNotifier {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  var _repository = NotificationsRepository();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  List<String> data = [];
 
-  final String title;
+  final int pagination = 20;
+  int limit;
+  int offset;
+  final scrollController = ScrollController();
+  bool loading = false;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  LoadMoreModel() {
+    setListener();
+    refresh();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  setListener() {
+    scrollController.addListener(() {
+      final max = scrollController.position.maxScrollExtent;
+      final offset = scrollController.offset;
+      if (max == offset) {
+        loadMore();
+      }
     });
   }
 
+  loadMore() async {
+    if (!loading) {
+      loading = true;
+      notifyListeners();
+
+      await getNotifications();
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<List<String>> refresh() async {
+    data.clear();
+    limit = pagination;
+    offset = 0;
+    await getNotifications();
+    return data;
+  }
+
+  Future<void> getNotifications() async {
+    try {
+      final notify = await _repository.fetchNotifications(limit: limit, offset: offset);
+      data += notify;
+      limit += pagination;
+      offset += pagination;
+    } catch (err) {
+    } finally {
+      notifyListeners();
+    }
+  }
+
+}
+
+class NotificationsRepository {
+
+  Future<List<String>> fetchNotifications({int limit, int offset}) async {
+
+    List<String> data = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+
+    ];
+
+    return Future.delayed(Duration(seconds: 1), () {
+      return Future.value(data);
+    });
+  }
+
+}
+
+class LoadMoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    Size screenSize = MediaQuery.of(context).size;
+    double headerHeight = 60;
+
+
+    return Consumer<LoadMoreModel>(
+        builder: (context, model, _) {
+
+          return Scaffold(
+            appBar: AppBar(
+                title: Text('both load more sample'),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    model.refresh();
+                  },
+                ),
+              ],
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            body: Center(
+              child: Column(
+                children: <Widget>[
+
+                  // header
+                  Container(
+                    width: screenSize.width,
+                    height: headerHeight,
+                    color: Colors.blue,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                            child: Text("header contents"),
+                            margin: EdgeInsets.only(left: 16)
+                        ),
+                      ],
+                    ),
+                  ),
+                  // header end
+
+                  Container(
+                    height: screenSize.height - headerHeight - 80 ,
+                    child: RefreshIndicator(
+                      child: ListView.builder(
+                        controller: model.scrollController,
+                        itemCount: model.data.length + 1,
+                        itemBuilder: (context, index) {
+
+                          if (model.data.isEmpty) {
+                            return  Center(child: CircularProgressIndicator());
+                          }
+
+                          // display listview data
+                          if (index < model.data.length) {
+                            final item = model.data[index];
+                            return ListTile(
+                              title: Text(item),
+                            );
+                          }
+
+                          // when data length is exceeded
+                          return Center(
+                            child: Opacity(
+                              opacity: model.loading ? 1.0 : 0.0,
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        },
+
+                      ),
+                      onRefresh: () async {
+                        await model.refresh();
+                      },
+                    ),
+                  ),
+
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        }
     );
   }
 }
+
+
